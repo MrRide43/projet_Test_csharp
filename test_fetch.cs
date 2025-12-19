fb2ad34c98dedb660f198ca27ce08bc00ec77f7c
Olivier Carlini <ocarlini@rfit-tech.com>
2025-12-19 11:42:53 +0100

    commit pour tester le fetch

---
+++ test_fetch.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+console.WriteLine("test pour faire un fetch");
+console.WriteLine("test pour faire un fetch");
+console.WriteLine("test pour faire un fetch");
+console.WriteLine("test pour faire un fetch");
+console.WriteLine("test pour faire un fetch");
+console.WriteLine("test pour faire un fetch");
+console.WriteLine("test pour faire un fetch");
+console.WriteLine("test pour faire un fetch");
+console.WriteLine("test pour faire un fetch");
+console.WriteLine("test pour faire un fetch");
